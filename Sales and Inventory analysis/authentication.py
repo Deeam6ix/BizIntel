f13c71d18164351e7475c81d516dbee664af19d2
Deeam6ix <deeamdarby@gmail.com>
2025-06-20 19:53:46 +0000

    first authentification commit

--- Sales and Inventory analysis/authentication.py
+++ Sales and Inventory analysis/authentication.py
@@ -1,2 +1,34 @@
+import re
+
 def main():
-    
+    user_info()
+
+def user_info():
+    user_choice=input("Would you like to\n" 
+    "1.Sign in?\n" 
+    "2.Create a account?\n")
+    if user_choice=="1":
+        ...
+    elif user_choice=="2":
+        try:
+            name=input("Please insert your name: ")
+            surname=input("Please insert your surname: ")
+            user_name=input("Please insert your username: ")
+            password=input("Please insert your password: ")
+        except KeyboardInterrupt:
+            print("Input was interrupted by user.")
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+        except ValueError:
+            print("Please enter a valid character or word.")
+        except EOFError:
+            print("Control commands are not allowed e.g(CRTL+D,CRTL+Z)")
+    """
+    -if there is a name 
+    -check if name is already in the system
+    -check if
+    """
+
+if __name__=="__main__":
+    main()
+

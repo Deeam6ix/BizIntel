7517a13ca7dc2dc6c966cffaf226def183299b8e
Deeam6ix <deeamdarby@gmail.com>
2025-06-21 21:20:01 +0200

    Create products_and_services.py

---
+++ products_and_services.py
@@ -0,0 +1,18 @@
+import random
+
+def main():
+    register_product_or_service():
+    
+def register_product_or_service():
+    name=input("Product or Service Name: ")
+    price=int(input("Product or Service Proce: ")
+    barcode()
+    
+def barcode():
+    unit=random.randint(0,9)
+    print(f"The Product or Service's assigned barcode: "
+    "{unit}{unit}{unit}{unit}{unit}{unit}{unit}{unit}{unit}"
+    "{unit}{unit}{unit}"
+    
+if __name__=="__main__":
+    main()

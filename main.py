22048d0e97938ab75b966927109db82127552610
Deeam6ix <deeamdarby@gmail.com>
2025-06-21 16:16:33 +0000

    cleaned up main

--- main.py
+++ main.py
@@ -2,8 +2,21 @@ import sales
 import authentication
 
 def main():
-    authentication.user_choice_one()
-    sales.main
-    
+    login()
+    sales.welcome_message()
+    sales.inventory_dashboard()
+
+def login():
+    account=input("Do you already have an account with us?\n"
+    "1.Yes\n"
+    "2.No\n\n"
+    ":")
+    if account=="1":
+        authentication.user_choice_one()
+    else:
+        authentication.user_choice_two()
+        print("\n\nPlease go ahead and login now.")
+        authentication.user_choice_one()
+
 if __name__=="__main__":
     main()
